473203e9a19bf23ccef27bddbcd3a5f59fe176a4
Lopez Uribe <marco.a.lopez.uribe@accenture.com>
2021-03-23 17:19:16 -0500

    select date works

--- src/test/java/com/workshop/TestSelenium.java
+++ src/test/java/com/workshop/TestSelenium.java
@@ -1,28 +1,26 @@
 package com.workshop;
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
 
 public class TestSelenium {
 
-    private String today;
+    private String date;
 
     @Test
     public void searchFlights() throws InterruptedException {
         String departureCity = "Medellin";
         String destinationCity = "Bogota";
-        today = getCurrentDay();
-        System.out.println("Today's number: " + today + "\n");
+        date = getDate();
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
@@ -36,30 +34,24 @@ public class TestSelenium {
         WebElement departureDate = driver.findElement(By.id("criteria-dates-from"));
         WebElement searchButton = driver.findElement(By.id("criteria-search-button-desktop"));
         oneWayCheckbox.click();
-        departure.sendKeys(departureCity + Keys.ENTER);
+        //departure.sendKeys(departureCity + Keys.ENTER);
         destination.sendKeys(destinationCity + Keys.ENTER);
         departureDate.click();
         Thread.sleep(2000);
-        WebElement dateWidgetFrom = driver.findElement(By.xpath("//*[@id='month-list']/div[1]/div[2]/table/tbody"));
-        List<WebElement> columns = dateWidgetFrom.findElements(By.tagName("td"));
-        for (WebElement cell : columns) {
-            if (cell.getText().equals(today)) {
-                cell.click();
-                break;
-            }
-        }
-        // String text = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[3]/div[2]/div[2]/" +
-        // "div/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div")).getText();
+        WebElement dateButton = driver.findElement(By.xpath(String.format("//button[@date='%s']", date)));
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", dateButton);
+        Thread.sleep(4000);
         searchButton.click();
         // driver.close();
         // driver.quit();
     }
 
-    private String getCurrentDay() {
-        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
-        int todayInt = calendar.get(Calendar.DAY_OF_MONTH);
-        String todayStr = Integer.toString(todayInt);
-        return todayStr;
+    private String getDate() {
+        String pattern = "yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        String date = simpleDateFormat.format(new Date());
+        return date;
     }
 }
 
